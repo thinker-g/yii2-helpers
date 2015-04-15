ecb3365fbb2daf3a01fc66b1d1f2883ee8405850
thinker-g <jiyan.guo@gmail.com>
2015-04-16 01:14:04 +0800

    Update getter and setters.

--- controllers/CrudController.php
+++ controllers/CrudController.php
@@ -6,10 +6,11 @@ use yii\web\Controller;
 /**
  *
  * @author Thinker_g
- * @property array $crudConfig Return controller module's "crudModels" attribute.
+ * @property array $crudConfig Return controller module's "crudConfig" attribute.
  */
 abstract class CrudController extends Controller
 {
+    public $moduleAttr = 'crudConfig';
     /**
      * @var array
      * A template of the array structure:
@@ -29,17 +30,26 @@ abstract class CrudController extends Controller
      */
     private $_crudConfig;
     /**
-     *
+     * Getter, if _crudConfig is null, try to get it from module attribute.
      * @param string $controllerID
      * @return array
      */
     public function getCrudConfig($controllerID = null)
     {
         if (is_null($this->_crudConfig)) {
-            $this->_crudConfig = $this->module->crudModels;
+            $this->_crudConfig = $this->module->{$this->moduleAttr};
         }
         return $controllerID && isset($this->_crudConfig[$controllerID]) ?
             $this->_crudConfig[$controllerID] : $this->_crudConfig;
+    }
+    
+    /**
+     * Setter
+     * @param array $config
+     */
+    public function setCrudConfig($config)
+    {
+        $this->_crudConfig = $config;
     }
 }
 
