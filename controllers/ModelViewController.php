383601cf3f206ba71a37fab4db939c0a15a8f5b2
thinker-g <jiyan.guo@gmail.com>
2015-05-04 04:08:21 +0800

    Update interface.

--- controllers/ModelViewController.php
+++ controllers/ModelViewController.php
@@ -32,15 +32,29 @@ abstract class ModelViewController extends Controller
     private $_mvMap;
     private $_lastActionID;
     private $_actionMvMap;
+
+    /**
+     *
+     * @param array $map
+     * @param string $targetKey
+     * @return array|null
+     */
+    public function assembleMap(&$map, $targetKey)
+    {
+        //TODO fix this.
+        return [];
+    }
+
     /**
      * Return model-view map by controller id.
-     * Leave $controllerID as null to fetch all maps indexed by controller ID.
+     * Leave $controllerID as null to fetch all maps indexed by the given controller ID.
+     * @param bool $renew
      * @param string $controllerID
      * @return array
      */
     public function getMvMap($renew = false, $controllerID = null)
     {
-        if (is_null($controllerID)) {
+        if (is_null($controllerID) || $controllerID == $this->id) {
             // Return current controller's mv map.
             if (is_null($this->_mvMap) || $renew) {
                 if (is_null($this->moduleAttr)) {
@@ -57,7 +71,7 @@ abstract class ModelViewController extends Controller
     }
 
     /**
-     * Setter
+     * Set mv map for ONLY current controller.
      * @param array $config
      */
     public function setMvMap($config)
@@ -67,82 +81,59 @@ abstract class ModelViewController extends Controller
 
     /**
      *
-     * @param string $key
-     * @param string $actionID
-     * @return string|array|null
-     */
-    public function getModelClass($key = self::KEY_MODEL, $actionID = null)
-    {
-        if (is_null($actionID)) {
-            // return current action's model class
-            return $this->getActionMvMap()[$key];
-        } elseif ($actionID === self::KEY_DEFAULT) {
-            // return default model class
-            return isset($this->getMvMap()[self::KEY_DEFAULT][$key]) ? $this->getMvMap()[self::KEY_DEFAULT][$key] : null;
-        } else {
-            // return other action's model class
-            return $this->getActionMvMap($actionID)[$key];
-        }
-
-    }
-
-    /**
-     *
      * @param string $actionID
      * @param bool $renew
+     * @param array $contextMap Controller level model-view map array.
      * @return array
      */
-    public function getActionMvMap($actionID = null, $renew = false)
+    public function getActionMvMap($actionID = null, $renew = false, $contextMap = null)
     {
         $actionID || $actionID = $this->action->id;
-        if ($renew || is_null($this->_actionMvMap) || $actionID != $this->_lastActionID) {
-            $this->_lastActionID = $actionID;
-            $this->_actionMvMap = $this->assembleMap($this->getMvMap(), $actionID);
-        }
-        return $this->_actionMvMap;
-    }
-    
-    /**
-     *
-     * @param array $map
-     * @param string $targetKey
-     * @return array|null
-     */
-    public function assembleMap(&$map, $targetKey)
-    {
-        if (isset($map[self::KEY_DEFAULT])) {
-            return isset($map[$targetKey]) ? ArrayHelper::merge($map[self::KEY_DEFAULT], $map[$targetKey]) : $map[self::KEY_DEFAULT];
+        if (is_null($contextMap)) {
+            if ($renew || is_null($this->_actionMvMap) || $actionID != $this->_lastActionID) {
+                $this->_lastActionID = $actionID;
+                $this->_actionMvMap = $this->assembleMap($this->getMvMap(), $actionID);
+            }
+            return $this->_actionMvMap;
         } else {
-            // No default map set
-            return isset($map[$targetKey]) ? $map[$targetKey] : [];
+            return $this->assembleMap($contextMap, $actionID)[$actionID];
         }
     }
 
     /**
+     *
+     * @param string $key
      * @param string $actionID
-     * @return Ambigous <multitype:, array, NULL, mixed, unknown>
+     * @param array $contextMap
+     * @return string|array|null
      */
-    public function getViewID($actionID = null, $renew = false)
+    public function getModelClass($key = self::KEY_MODEL, $actionID = null, $contextMap = null)
     {
-        $actionID || $actionID = $this->action->id;
-        if (
-            isset($this->getActionMvMap($actionID, $renew)[self::KEY_VIEW])
-            && !empty($this->getActionMvMap($actionID, $renew)[self::KEY_VIEW])
-        ) {
-            return $this->getActionMvMap($actionID, $renew)[self::KEY_VIEW];
-        } else {
-            return $actionID;
-        }
+        return $this->getActionMvMap($actionID, false, $contextMap)[$key];
+    }
+
+    /**
+     * @param string $actionID
+     * @param bool $renew
+     * @param array $contextMap
+     * @return string
+     */
+    public function getViewID($actionID = null, $renew = false, $contextMap = null)
+    {
+        $targetViewID = $this->getActionMvMap($actionID, $renew, $contextMap)[self::KEY_VIEW];
+        return empty($targetViewID) ? $this->action->id : $targetViewID;
     }
 
     /**
      * @param string $id
+     * @param string $actionID
+     * @param array $contextMap
      * @throws NotFoundHttpException
      * @return \yii\db\ActiveRecordInterface
      */
-    protected function findModel($id, $actionID = null)
+    protected function findModel($id, $actionID = null, $contextMap = null)
     {
-        if (is_array($modelClass = $this->getModelClass(self::KEY_MODEL, $actionID))) {
+        if (is_array($modelClass = $this->getModelClass(self::KEY_MODEL, $actionID, $contextMap))) {
             $modelClass = $modelClass['class'];
         }
         if (($model = $modelClass::findOne($id)) !== null) {
