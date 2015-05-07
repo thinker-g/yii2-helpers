3ba1056e0c37d6886c38cce9bb311de90fc41e7c
Yann GUO <yann.guo@tlscontact.com>
2015-05-07 17:55:10 +0800

    Add NSedModuleViewPath.

---
+++ traits/NSedModuleViewPath.php
@@ -0,0 +1,40 @@
+<?php
+namespace thinker_g\Helpers\traits;
+
+/**
+ * When using this trait, a private attribute [[_nsedModuleViewPath]] will be added to the host class.
+ * The class's code should never try to access this attribute.
+ * @author tlsadmin
+ */
+trait NSedModuleViewPath
+{
+    private $_nsedModuleViewPath;
+
+    /**
+     * @inheritdoc
+     * @see \yii\base\Module::setViewPath($path)
+     */
+    public function setViewPath($path)
+    {
+        $this->_nsedModuleViewPath = $path;
+        parent::setViewPath($path);
+    }
+    
+    /**
+     * If the viewPath is not explicitly set for in this module's configuration,
+     * it will return default "[[basePath]]/views/(back|front)".
+     * @inheritdoc
+     * @see \yii\base\Module::getViewPath()
+     */
+    public function getViewPath()
+    {
+        return is_null($this->_nsedModuleViewPath) ?
+        parent::getViewPath()
+        . DIRECTORY_SEPARATOR
+        . array_pop(explode('\\', $this->controllerNamespace)) :
+        parent::getViewPath();
+    }
+
+}
+
+?>
